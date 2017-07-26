06f460f8ac477609769378e16c448d207c7e68c5
Jeremy Poulin <jpoulin@redhat.com>
2017-07-26 15:25:54 -0400

    Added slave lines to init.sh

--- setup/init.sh
+++ setup/init.sh
@@ -2,3 +2,5 @@ oc new-project multiarch-aos;
 oc new-app jenkins-persistent;
 oc create -f templates/multiarch-aos-origin-template.yml
 oc create -f templates/multiarch-aos-e2e-template.yml
+oc create -f templates/multiarch-aos-jenkins-slave-template.yml
+oc new-app multiarch-aos-jenkins-slave-builder
