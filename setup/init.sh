94ac8b79543a5b8e0ec816eb70f5fb7ad2d25461
Jeremy Poulin <jpoulin@redhat.com>
2017-08-03 15:18:39 +0000

    Updated init script to point at the correct build template.

--- setup/init.sh
+++ setup/init.sh
@@ -3,4 +3,4 @@ oc new-app jenkins-persistent;
 oc create -f templates/multiarch-aos-origin-template.yml
 oc create -f templates/multiarch-aos-e2e-template.yml
 oc create -f templates/multiarch-aos-jenkins-slave-template.yml
-oc new-app multiarch-aos-jenkins-slave-builder
+oc new-app jenkins-slave-builder
