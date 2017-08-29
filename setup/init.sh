cb5531ccc4d01c4e0679ce4d22db93b400b444fb
Jeremy Poulin <jpoulin@redhat.com>
2017-08-29 20:19:17 +0000

    Pushing updated init scripts

--- setup/init.sh
+++ setup/init.sh
@@ -1,6 +1,7 @@
-oc new-project multiarch-aos;
-oc new-app jenkins-persistent;
-oc create -f templates/multiarch-aos-origin-template.yml
-oc create -f templates/multiarch-aos-e2e-template.yml
-oc create -f templates/multiarch-aos-jenkins-slave-template.yml
-oc new-app jenkins-slave-builder
+sudo oc login -u developer
+sudo oc new-project multiarch-aos;
+sudo oc new-app jenkins-persistent;
+sudo oc create -f templates/multiarch-aos-origin-template.yml
+sudo oc create -f templates/multiarch-aos-e2e-template.yml
+sudo oc create -f templates/multiarch-aos-jenkins-slave-template.yml
+sudo oc new-app jenkins-slave-builder
---
+++ setup/install.sh
@@ -0,0 +1,24 @@
+#bash
+sudo yum install -y git docker firewalld;
+curl -L -O https://github.com/openshift/origin/releases/download/v3.6.0/openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit.tar.gz
+tar xvzf openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit.tar.gz;
+sudo cp openshift-origin-client-tools-v3.6.0-c4dd4cf-linux-64bit/oc /bin;
+
+# Start Services
+sudo systemctl enable docker;
+sudo systemctl enable firewalld;
+sudo systemctl start docker;
+sudo systemctl start firewalld;
+
+# Allow OpenShift docker registry
+echo "INSECURE_REGISTRY='--insecure-registry 172.30.0.0/16'" | sudo tee -a /etc/sysconfig/docker;
+echo "{ \"mtu\": 1400 }" | sudo tee /etc/docker/daemon.json;
+
+# Open up ports for connections to docker
+sudo firewall-cmd --permanent --new-zone dockerc;
+sudo firewall-cmd --permanent --zone dockerc --add-source 172.17.0.0/16;
+sudo firewall-cmd --permanent --zone dockerc --add-port 8443/tcp;
+sudo firewall-cmd --permanent --zone dockerc --add-port 53/udp;
+sudo firewall-cmd --permanent --zone dockerc --add-port 8053/udp;
+sudo firewall-cmd --permanent --zone dockerc --add-port 50000/tcp;
+sudo firewall-cmd --reload;
