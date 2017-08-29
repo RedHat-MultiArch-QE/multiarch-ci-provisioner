d26b04d32ea93debee1352816fba77e4fc4c19af
Jeremy Poulin <jpoulin@redhat.com>
2017-08-29 20:19:17 +0000

    Pushing updated init scripts

--- setup/clusterup.sh
+++ setup/clusterup.sh
@@ -1,3 +1,6 @@
+#bash
+
 sudo iptables -F;
 sudo service docker restart;
-oc cluster up --public-hostname=10.8.173.196 --routing-suffix=10.8.173.196.xip.io;
+sudo oc cluster up --public-hostname=10.8.174.97 --routing-suffix=10.8.174.97.xip.io;
+
