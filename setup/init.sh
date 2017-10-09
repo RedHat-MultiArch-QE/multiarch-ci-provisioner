d60c733875f2d5cd1f2ead22bda22e206893338b
Jeremy Poulin <jpoulin@redhat.com>
2017-10-09 14:38:12 -0400

    fixup automation of privileged containers

--- setup/init.sh
+++ setup/init.sh
@@ -6,4 +6,6 @@ oc create -f templates/provisioner-template.yml
 oc create -f templates/provision-multiarch-slave-template.yml
 oc create -f templates/teardown-multiarch-slave-template.yml
 oc new-app provisioner-builder
-oc adm policy add-scc-to-user priviledged default
+oc login -u system:admin
+oc adm policy add-scc-to-user privileged default
+oc login -u developer
