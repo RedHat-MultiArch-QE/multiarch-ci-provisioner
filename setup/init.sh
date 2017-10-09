a6359c90451c16700f56b54b7b2559363a8422ce
Jeremy Poulin <jpoulin@redhat.com>
2017-10-09 14:54:16 -0400

    Tweaked scc to allow priviledged developer

--- setup/init.sh
+++ setup/init.sh
@@ -7,5 +7,5 @@ oc create -f templates/provision-multiarch-slave-template.yml
 oc create -f templates/teardown-multiarch-slave-template.yml
 oc new-app provisioner-builder
 oc login -u system:admin
-oc adm policy add-scc-to-user privileged default
+oc adm policy add-scc-to-user privileged developer
 oc login -u developer
