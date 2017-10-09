e945c6d636bd1b1fee09a4fdcd0ab82386cfc7cd
Jeremy Poulin <jpoulin@redhat.com>
2017-10-09 13:32:51 -0400

    Renaming multiarch slave

--- setup/init.sh
+++ setup/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 oc login -u developer
-oc new-project multiarch-aos;
+oc new-project multiarch-provisioner;
 oc create -f templates/multiarch-origin-build-template.yml
+oc create -f templates/provisioner-template.yml
 oc create -f templates/provision-multiarch-slave-template.yml
-oc create -f templates/multiarch-slave-provisioner-builder-template.yml
-oc new-app jenkins-slave-builder
+oc create -f templates/teardown-multiarch-slave-template.yml
+oc new-app provisioner-builder
