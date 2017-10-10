afff16e9e8b8b85e5ec02cc8e4643c83ba0d83ee
Jeremy Poulin <jpoulin@redhat.com>
2017-10-10 17:59:39 +0000

    Added nohup to oc cluster up so I could logout safely

--- setup/clusterup.sh
+++ setup/clusterup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-echo "Enter the IP address of the new cluster:"
-read ip_address
-route_address=$ip_address".xip.io"
+# Ensure current user is in the docker group
+group=docker
+if [ $(id -gn) != $group ]; then
+  exec sg $group "$0 $*"
+fi
+
+echo "Enter the IP address of the new cluster:";
+read ip_address;
+route_address=$ip_address".xip.io";
 
 sudo iptables -F;
 sudo service docker restart;
-oc cluster up --public-hostname=$ip_address --routing-suffix=$route_address;
+nohup oc cluster up --public-hostname=$ip_address --routing-suffix=$route_address;
 
 exit 0
