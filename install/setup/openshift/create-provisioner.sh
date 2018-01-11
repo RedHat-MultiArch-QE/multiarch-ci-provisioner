b4ef3f85766d016ce43dd70178d6826bef4075bc
Jeremy Poulin <jpoulin@redhat.com>
2018-01-11 16:40:42 -0500

    Moving secrets out to Jenkins instance

--- install/setup/openshift/create-provisioner.sh
+++ install/setup/openshift/create-provisioner.sh
@@ -2,20 +2,8 @@
 oc login -u developer
 oc create -f templates/provisioner-template.yml
 
-# Read Kerberos Service Tenant
-read -p "Kerberos Service Tenant: " ktenant
-
-# Read Jenkins username
-read -p "Jenkins Username: " username
-
-# Read password
-stty -echo
-read -p "Jenkins Token: " token; echo
-stty echo
-
-oc create secret generic beaker --from-file images/provisioner/secrets/beaker/client.conf --from-file images/provisioner/secrets/beaker/id_rsa 
-oc create secret generic krb5 --from-file images/provisioner/secrets/krb5/krb5.conf --from-file images/provisioner/secrets/krb5/krb5.keytab --from-literal tenant=${ktenant}
-oc create secret generic jenkins --from-literal username=$username --from-literal password=$token
+oc create secret generic beaker --from-file images/provisioner/secrets/beaker/client.conf
+oc create secret generic krb5 --from-file images/provisioner/secrets/krb5/krb5.conf
 
 oc new-app provisioner-builder
 oc login -u system:admin
