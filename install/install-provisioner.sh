eccd60a8a53e3612d043dae28229b2af053ed9fb
Jeremy Poulin <jpoulin@redhat.com>
2018-01-12 12:56:43 -0500

    Expanding installed options

--- install/install-provisioner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#
-# Use this file to setup the OpenShift provisioner in a dedicated VM.
-root_dir=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )/..
-cd $root_dir
-
-# Initialize projects within cluster
-bash $root_dir/install/setup/openshift/create-provisioner.sh
-
