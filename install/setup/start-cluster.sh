6e37724f4fd112cefb1497bde3fe9820f20f3b6d
Jeremy Poulin <jpoulin@redhat.com>
2018-01-23 14:12:34 -0500

    Adding directory to persist project data

--- install/setup/start-cluster.sh
+++ install/setup/start-cluster.sh
@@ -12,6 +12,6 @@ route_address=$ip_address".xip.io";
 
 sudo iptables -F;
 sudo service docker restart;
-nohup oc cluster up --public-hostname=$ip_address --routing-suffix=$route_address;
+nohup oc cluster up --public-hostname=$ip_address --routing-suffix=$route_address --host-data-dir=$HOME/origin;
 
 exit 0
