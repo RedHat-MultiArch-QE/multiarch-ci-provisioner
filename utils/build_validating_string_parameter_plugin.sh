df21f4b896cc8426ffd0eb80b7a099ee57be9911
Jeremy Poulin <jpoulin@redhat.com>
2018-11-29 15:50:08 -0500

    Release v1.2.1 (#35)
    * Updates for v1.2.1 (#33)
    
    * remove deprecated install mode
    
    * add email-ext plugin
    
    * Version increment, ansible update, and increased gather_facts timeout. (#27)
    
    * Increased Ansible gather_facts timeout
    
    * Incrementing version number
    
    * Upgrading Ansible to 2.7.0
    
    * Adding central ci master plugins list (#28)
    
    * Rename central-ci-master-plugins.txt to 2.121.3-central-ci-master-plugins.txt
    
    * Updated build of validating string param plugin (#29)
    
    * Upgrading LinchPin (#30)
    
    * Downgrading to LinchPin v1.6.3
    
    * Fixed bug with gather timeout being updated to wrong env var (#34)
    
    * Revert "Revert "remove deprecated install mode""
    
    This reverts commit f3d0b08d6c2686721527dd5ee73fc368e4a8b28a.
    
    * Revert "Revert "add email-ext plugin""
    
    This reverts commit 94bc3d3efbe0f386c024da7ac3ef90808619fea5.

--- utils/build_validating_string_parameter_plugin.sh
+++ utils/build_validating_string_parameter_plugin.sh
@@ -3,6 +3,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 work_dir=$(pwd)
 root_dir=$(realpath "$(pwd)/..")
 
+plugin_version=2.4
+
 mkdir tmp
 cd tmp
 curl -sSO http://apache.mirrors.lucidnetworks.net/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
@@ -12,10 +14,10 @@ gpg --verify apache-maven-3.5.3-bin.tar.gz.asc apache-maven-3.5.3-bin.tar.gz
 sudo tar xzvf apache-maven-3.5.3-bin.tar.gz -C /opt
 export PATH=$PATH:/opt/apache-maven-3.5.3/bin
 
-git clone --depth 1 --branch validating-string-parameter-2.4.1 https://github.com/jlelli/validating-string-parameter-plugin.git
+git clone --depth 1 --branch validating-string-parameter-$plugin_version https://github.com/jenkinsci/validating-string-parameter-plugin
 cd validating-string-parameter-plugin
 mvn clean compile hpi:hpi
-mkdir -p $root_dir/s2i/jenkins/master/plugins
-cp target/validating-string-parameter.hpi $root_dir/s2i/jenkins/master/plugins/
+mkdir -p $root_dir/utils/plugins
+cp target/validating-string-parameter.hpi $root_dir/utils/plugins/validating-string-parameter-$plugin_version.hpi
 cd $work_dir
 rm -rf tmp
